078bfc85342c78fdebfced205269f6d1c8d38f66
Milind_77 <milinddhote77@gmail.com>
2021-09-30 09:05:20 +0530

    [Milind] Add : simple pattern with 5 rows and 10 col.

---
+++ Pattern_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pattern_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i <= 5; i++) //for rows
+            {
+                for (j = 1; j <= 10; j++) //for col
+                {
+                    Console.Write("-");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
